221fea0cb589dda589dde7bbcf3897624264faf7
amirkbug <amirworkplacett@gmail.com>
2025-02-03 16:38:58 +0330

    Final

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,34 @@
 from django.contrib import admin
-from .models import Personaltoken
+from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Personaltoken)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "is_staff")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    ordering = ("email",)
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
